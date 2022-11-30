681303aa3b614a0fc5a0394c4f041b213a1df563
Marco Colli <collimarco91@gmail.com>
2022-11-30 15:18:02 +0100

    Use HMAC-SHA256 for uid signature instead of HMAC-SHA1 (action required: go to Pushpad project settings and change the algorithm)

--- lib/Pushpad.php
+++ lib/Pushpad.php
@@ -8,7 +8,7 @@ class Pushpad {
 
   public static function signature_for($data) {
     if (!isset(self::$auth_token)) throw new \Exception('You must set Pushpad\Pushpad::$auth_token');
-    return hash_hmac('sha1', $data, self::$auth_token);
+    return hash_hmac('sha256', $data, self::$auth_token);
   }
 
 }
